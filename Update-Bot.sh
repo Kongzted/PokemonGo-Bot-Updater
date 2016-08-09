60c53324c03062efd409c50608118da528f7b4f1
Jonas Kongsted <mrjonezk@gmail.com>
2016-08-09 17:00:07 +0200

    Changelog v1.1
    Added backup function
    Fixed misspelling

--- Update-Bot.sh
+++ Update-Bot.sh
@@ -6,20 +6,30 @@
 #
 #
 # Made for: (Ar1i) - https://github.com/Ar1i/PokemonGo-Bot
-# Made by: (Kongzted Development) - 
+# Made by: (Kongzted Development) - https://github.com/KongztedDev
+#
+# DO NOT CHANGE ANYTHING IN THIS FILE! - DO NOT CHANGE ANYTHING IN THIS FILE! - DO NOT CHANGE ANYTHING IN THIS FILE! - DO NOT CHANGE ANYTHING IN THIS FILE!
 
-
-
+#Variables
+DATE=`date +%d-%m-%Y_%H:%M`
 ALIAS="[PGB Updater] "
 
+#Code
 if [ -d PokemonGo-Bot/ ]; then                                              # IF PGB UPDATER NEEDS TO UPDATE - NOT FIRST RUN
-    echo $ALIAS "Creating 'tmp' foler."
-    mkdir tmp/
-    echo $ALIAS "Copying Config file to tmp folder."
-    cp PokemonGo-Bot/Builds\ Only/Configs/Config.txt tmp/
+    echo $ALIAS "Creating 'temp' folder."
+    mkdir temp/
+    echo $ALIAS "Copying Config file to temp folder."
+    cp PokemonGo-Bot/Builds\ Only/Configs/Config.txt temp/
     sleep 2
-    echo $ALIAS "Removing old master folder."
-    rm -rf PokemonGo-Bot/
+	if [ -d backups/ ]; then
+	    echo $ALIAS "Backing up old master folder."
+		mv PokemonGo-Bot backups/PokemonGo-Bot_$DATE
+	else
+		echo $ALIAS "Creating 'backups' folder."
+		mkdir backups
+		echo $ALIAS "Backing up old master folder."
+		mv PokemonGo-Bot backups/PokemonGo-Bot_$DATE
+	fi
     sleep 2
     echo $ALIAS "Downloading update."
     git clone https://github.com/Ar1i/PokemonGo-Bot.git
@@ -28,13 +38,11 @@ if [ -d PokemonGo-Bot/ ]; then                                              # IF
     mkdir PokemonGo-Bot/Builds\ Only/Configs
     sleep 2
     echo $ALIAS "Replacing old config file into new config folder."
-    cp tmp/Config.txt PokemonGo-Bot/Builds\ Only/Configs/Config.txt
+    cp temp/Config.txt PokemonGo-Bot/Builds\ Only/Configs/Config.txt
     sleep 2
-    echo $ALIAS "Removing 'tmp' folder."
-    rm -rf tmp/
-    echo $ALIAS "Opening ReadME page."
-    start http://kongzted.net/projekter/PGB/get-started.html
-    echo $ALIAS "Done."
+    echo $ALIAS "Removing 'temp' folder."
+    rm -rf temp/
+    echo $ALIAS "Done. Enjoy your updated PokemonGo Bot!"
     sleep 5
 else                                                                        # IF PGB UPDATER NEEDS TO DOWNLOAD PGB - FIRST TIME
     echo $ALIAS "Downloading the latest version of PokemonGo-Bot."
@@ -42,10 +50,18 @@ else                                                                        # IF
     sleep 2
     echo $ALIAS "Creating 'configs' folder."
     mkdir PokemonGo-Bot/Builds\ Only/Configs
+	echo $ALIAS "Creating 'Config.txt' file."
     touch PokemonGo-Bot/Builds\ Only/Configs/Config.txt
     sleep 2
-    echo $ALIAS "Opening 'Get Started' page."
+	echo $ALIAS "Do you have Git Bash installed? Y/N"
+	select yn in "Yes" "No"; do
+		case $yn in
+			Yes ) sleep 2; break;;
+			No ) start https://git-scm.com/download/win; break;;
+		esac
+	done
+    echo $ALIAS "Opening getting started page."
     start http://kongzted.net/projekter/PGB/get-started.html
-    echo $ALIAS "Done. Enjoy your PokemonGo-Bot!"
+    echo $ALIAS "Done. Enjoy your PokemonGo Bot!"
     sleep 5
 fi
